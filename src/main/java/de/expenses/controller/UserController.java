958daf985714e7dba82d87db9b73c66a70bbb5c2
Alexander Kiemle <mail@alexander-kiemle.de>
2025-06-20 14:25:24 +0200

    add SLF4j logging

--- src/main/java/de/expenses/controller/UserController.java
+++ src/main/java/de/expenses/controller/UserController.java
@@ -2,6 +2,8 @@ package de.expenses.controller;
 
 import de.expenses.model.User;
 import de.expenses.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	private final UserRepository userRepo;
 
@@ -18,11 +22,13 @@ public class UserController {
 
 	@GetMapping
 	public List<User> getAllUsers() {
+		logger.info("get all users");
 		return userRepo.findAll();
 	}
 
 	@PostMapping
 	public User createUser(@RequestBody User user) {
+		logger.info("create a new user");
 		return userRepo.save(user);
 	}
 }
