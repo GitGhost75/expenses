1bc5b90f73b86c4935dfb87a2a7c6b4ed01b6eb6
Alexander Kiemle <mail@alexander-kiemle.de>
2025-06-20 16:51:34 +0200

    moved to folder /backend

--- backend/src/main/java/de/expenses/ExpensesApp.java
+++ backend/src/main/java/de/expenses/ExpensesApp.java
@@ -1,14 +1,18 @@
 package de.expenses;
 
+import de.expenses.configuration.CorsProperties;
+import de.expenses.configuration.ServerProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @OpenAPIDefinition(
-		info = @Info(title = "Expenses API", version = "1.0", description = "Verwalte Gruppen-Ausgaben")
+		info = @Info(title = "Expenses API", version = "0.2.0", description = "Verwalte Gruppen-Ausgaben")
 )
 @SpringBootApplication
+@EnableConfigurationProperties({CorsProperties.class, ServerProperties.class})
 public class ExpensesApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensesApp.class, args);
