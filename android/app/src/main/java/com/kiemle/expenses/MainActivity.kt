9f04e565abe0a3c763d49957aa81da2f815c598d
Alexander Kiemle <mail@alexander-kiemle.de>
2025-06-23 12:10:06 +0200

    initial revision android WebView-App

---
+++ android/app/src/main/java/com/kiemle/expenses/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kiemle.expenses
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kiemle.expenses.ui.theme.ExpensesTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: android.graphics.Bitmap?) {
+                // z. B. ProgressBar sichtbar machen
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                // z. B. ProgressBar ausblenden
+            }
+        }
+
+        webView.loadUrl("https://our-expenses.duckdns.org") // ← Deine URL hier einsetzen
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
